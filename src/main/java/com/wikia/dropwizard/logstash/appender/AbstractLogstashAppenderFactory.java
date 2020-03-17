253083b439f044fee7bb8e70cf20c6141977c8ca
agnieszka.janik <agnieszka.janik@beekeeper.io>
2020-03-17 15:13:09 +0100

    upgrade to newst gradle wrapper add beekeeper plugin apply beekeeeper formatting upgadle dependencies

--- src/main/java/com/wikia/dropwizard/logstash/appender/AbstractLogstashAppenderFactory.java
+++ src/main/java/com/wikia/dropwizard/logstash/appender/AbstractLogstashAppenderFactory.java
@@ -10,90 +10,90 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 abstract class AbstractLogstashAppenderFactory extends AbstractAppenderFactory {
-  @NotNull
-  protected String host;
+    @NotNull
+    protected String host;
 
-  @Min(1)
-  @Max(65535)
-  protected int port;
+    @Min(1)
+    @Max(65535)
+    protected int port;
 
-  protected boolean includeCallerData = false;
+    protected boolean includeCallerData = false;
 
-  protected boolean includeContext = true;
+    protected boolean includeContext = true;
 
-  protected boolean includeMdc = true;
+    protected boolean includeMdc = true;
 
-  protected HashMap<String, String> customFields;
+    protected HashMap<String, String> customFields;
 
-  protected HashMap<String, String> fieldNames;
+    protected HashMap<String, String> fieldNames;
 
-  @JsonProperty
-  public void setHost(String host) {
-    this.host = host;
-  }
+    @JsonProperty
+    public void setHost(String host) {
+        this.host = host;
+    }
 
-  @JsonProperty
-  public String getHost() {
-    return host;
-  }
+    @JsonProperty
+    public String getHost() {
+        return host;
+    }
 
-  @JsonProperty
-  public void setPort(int port) {
-    this.port = port;
-  }
+    @JsonProperty
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-  @JsonProperty
-  public int getPort() {
-    return port;
-  }
+    @JsonProperty
+    public int getPort() {
+        return port;
+    }
 
-  @JsonProperty
-  public boolean getIncludeCallerData() {
-    return includeCallerData;
-  }
+    @JsonProperty
+    public boolean getIncludeCallerData() {
+        return includeCallerData;
+    }
 
-  @JsonProperty
-  public void setIncludeCallerData(boolean includeCallerData) {
-    this.includeCallerData = includeCallerData;
-  }
+    @JsonProperty
+    public void setIncludeCallerData(boolean includeCallerData) {
+        this.includeCallerData = includeCallerData;
+    }
 
-  @JsonProperty
-  public boolean getIncludeContext() {
-    return includeContext;
-  }
+    @JsonProperty
+    public boolean getIncludeContext() {
+        return includeContext;
+    }
 
-  @JsonProperty
-  public void setIncludeContext(boolean includeContext) {
-    this.includeContext = includeContext;
-  }
+    @JsonProperty
+    public void setIncludeContext(boolean includeContext) {
+        this.includeContext = includeContext;
+    }
 
-  @JsonProperty
-  public boolean getIncludeMdc() {
-    return includeMdc;
-  }
+    @JsonProperty
+    public boolean getIncludeMdc() {
+        return includeMdc;
+    }
 
-  @JsonProperty
-  public void setIncludeMdc(boolean includeMdc) {
-    this.includeMdc = includeMdc;
-  }
+    @JsonProperty
+    public void setIncludeMdc(boolean includeMdc) {
+        this.includeMdc = includeMdc;
+    }
 
-  @JsonProperty
-  public HashMap<String, String> getCustomFields() {
-    return customFields;
-  }
+    @JsonProperty
+    public HashMap<String, String> getCustomFields() {
+        return customFields;
+    }
 
-  @JsonProperty
-  public void setCustomFields(HashMap<String, String> customFields) {
-    this.customFields = customFields;
-  }
+    @JsonProperty
+    public void setCustomFields(HashMap<String, String> customFields) {
+        this.customFields = customFields;
+    }
 
-  @JsonProperty
-  public HashMap<String, String> getFieldNames() {
-    return fieldNames;
-  }
+    @JsonProperty
+    public HashMap<String, String> getFieldNames() {
+        return fieldNames;
+    }
 
-  @JsonProperty
-  public void setFieldNames(HashMap<String, String> fieldNames) {
-    this.fieldNames = fieldNames;
-  }
+    @JsonProperty
+    public void setFieldNames(HashMap<String, String> fieldNames) {
+        this.fieldNames = fieldNames;
+    }
 }
--- src/main/java/com/wikia/dropwizard/logstash/appender/LogstashSocketAppenderFactory.java
+++ src/main/java/com/wikia/dropwizard/logstash/appender/LogstashSocketAppenderFactory.java
@@ -11,38 +11,42 @@ import net.logstash.logback.appender.LogstashSocketAppender;
 
 @JsonTypeName("logstash-socket")
 public class LogstashSocketAppenderFactory extends AbstractLogstashAppenderFactory {
-  public LogstashSocketAppenderFactory() {
-    port = SyslogConstants.SYSLOG_PORT;
-  }
-
-  @Override
-  public Appender build(LoggerContext context, String s, LayoutFactory layoutFactory,
-                        LevelFilterFactory levelFilterFactory,
-                        AsyncAppenderFactory asyncAppenderFactory) {
-    final LogstashSocketAppender appender = new LogstashSocketAppender();
-
-    appender.setName("logstash-socket-appender");
-    appender.setContext(context);
-    appender.setHost(host);
-    appender.setPort(port);
-
-    appender.setIncludeCallerData(includeCallerData);
-    appender.setIncludeMdc(includeMdc);
-    appender.setIncludeContext(includeContext);
-
-    if (customFields != null) {
-      LogstashAppenderFactoryHelper
-          .getCustomFieldsFromHashMap(customFields)
-          .ifPresent(appender::setCustomFields);
+    public LogstashSocketAppenderFactory() {
+        port = SyslogConstants.SYSLOG_PORT;
     }
 
-    if (fieldNames != null) {
-      appender.setFieldNames(LogstashAppenderFactoryHelper.getFieldNamesFromHashMap(fieldNames));
+    @Override
+    public Appender build(
+            LoggerContext context,
+            String s,
+            LayoutFactory layoutFactory,
+            LevelFilterFactory levelFilterFactory,
+            AsyncAppenderFactory asyncAppenderFactory
+    ) {
+        final LogstashSocketAppender appender = new LogstashSocketAppender();
+
+        appender.setName("logstash-socket-appender");
+        appender.setContext(context);
+        appender.setHost(host);
+        appender.setPort(port);
+
+        appender.setIncludeCallerData(includeCallerData);
+        appender.setIncludeMdc(includeMdc);
+        appender.setIncludeContext(includeContext);
+
+        if (customFields != null) {
+            LogstashAppenderFactoryHelper
+                .getCustomFieldsFromHashMap(customFields)
+                .ifPresent(appender::setCustomFields);
+        }
+
+        if (fieldNames != null) {
+            appender.setFieldNames(LogstashAppenderFactoryHelper.getFieldNamesFromHashMap(fieldNames));
+        }
+
+        appender.addFilter(levelFilterFactory.build(threshold));
+        appender.start();
+
+        return wrapAsync(appender, asyncAppenderFactory);
     }
-
-    appender.addFilter(levelFilterFactory.build(threshold));
-    appender.start();
-
-    return wrapAsync(appender, asyncAppenderFactory);
-  }
 }
